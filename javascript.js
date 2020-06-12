d15f19549c35a2e06b16d5a82ec009877de2da33
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-12 00:32:50 -0400

    Add create element for button and textarea

--- javascript.js
+++ javascript.js
@@ -1,3 +1,9 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 document.getElementById('currentDay').textContent = days[new Date().getDay()] + ", " + new Date().getDate() + " " + months[new Date().getMonth()] + ", " + new Date().getFullYear();
+var timeBlocks = document.getElementById('time-block');
+var textArea = document.createElement('textarea');
+var button = document.createElement("button");
+timeBlocks.appendChild(textArea);
+timeBlocks.appendChild(button);
+timeBlocks.children[1].classList.add('saveBtn')
