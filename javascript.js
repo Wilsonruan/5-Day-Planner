33c22fff1d726d069f8f84c9993fe4814d74eea0
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-11 23:55:52 -0400

    Show current date with format

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
-var currentDate = new Date(year, month, day);
-var getDate = document.getElementById('currentDay')
-getDate.textContent = "Today";
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById('currentDay').textContent = days[new Date().getDay()] + ", " + new Date().getDate() + " " + months[new Date().getMonth()] + ", " + new Date().getFullYear();
