4800bf3ade1817e2009fd4dabe729c23a8acc9e4
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-12 21:07:58 -0400

    Create a row for 1 hour and adjust the styles.css

--- javascript.js
+++ javascript.js
@@ -2,8 +2,9 @@ var months = ["January", "February", "March", "April", "May", "June", "July", "A
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 document.getElementById('currentDay').textContent = days[new Date().getDay()] + ", " + new Date().getDate() + " " + months[new Date().getMonth()] + ", " + new Date().getFullYear();
 var timeBlocks = document.getElementById('time-block');
-var textArea = document.createElement('textarea');
-var button = document.createElement("button");
-timeBlocks.appendChild(textArea);
-timeBlocks.appendChild(button);
-timeBlocks.children[1].classList.add('saveBtn')
+timeBlocks.appendChild(document.createElement('p'));
+timeBlocks.appendChild(document.createElement('textarea'));
+timeBlocks.appendChild(document.createElement("button"));
+timeBlocks.children[0].classList.add('hour', 'col-1');
+timeBlocks.children[1].classList.add('row', 'col-10');
+timeBlocks.children[2].classList.add('saveBtn', 'col-1');
