a5e009dd1ddea5254a04a94f48c215317c382b3b
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-13 15:00:49 -0400

    Convert the rest to jQuery

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
-var timeBlocks = document.getElementById('time-block')
+// var timeBlocks = document.getElementById('time-block')
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
-document.getElementById('currentDay').textContent = days[new Date().getDay()] + ", " + new Date().getDate() + " " + months[new Date().getMonth()] + ", " + new Date().getFullYear();
+// document.getElementById('currentDay').textContent = days[new Date().getDay()] + ", " + new Date().getDate() + " " + months[new Date().getMonth()] + ", " + new Date().getFullYear();
+$('#currentDay').text(days[new Date().getDay()] + ", " + new Date().getDate() + " " + months[new Date().getMonth()] + ", " + new Date().getFullYear());
 var currentTime = new Date().getHours();
 var arrayDayPlanner = JSON.parse(localStorage.getItem("arrayDayPlanner"));
 
@@ -11,7 +12,8 @@ if (arrayDayPlanner === null) {
 }
 
 for (var i = 0; i < 9; i++) {
-    $('#time-block').append('<form method="POST" class="container d-flex justify-content-center"> <label class="hour col-2 col-sm-1"> <br> <p>' + hours[i] + '</p> </label> <textarea id="' + i + '" class="row col-8 col-sm-10"></textarea> <button class="saveBtn col-2 col-sm-1">Save</button> </form>')
+    $('#time-block').append('<form method="POST" class="container d-flex justify-content-center"> <label class="hour col-2 col-sm-1"> <br> <p>' + hours[i] + '</p> </label> <textarea id="textarea' + i + '" class="row col-8 col-sm-10"></textarea> <button id="' + i + '" class="saveBtn col-2 col-sm-1">Save</button> </form>')
+    $('#textarea' + i).text(arrayDayPlanner[i])
     // var timeBlocksInside = timeBlocks.appendChild(document.createElement('form'));
     // timeBlocksInside.setAttribute('id',i)
     // timeBlocksInside.setAttribute('method',"POST")
@@ -37,20 +39,29 @@ for (var i = 0; i < 9; i++) {
     // }
 
     if (i + 9 == currentTime) {
-        $('#'+ i).addClass('present')
+        $('#textarea'+ i).addClass('present')
     } else if (currentTime < i + 9) {
-        $('#'+ i).addClass('future')
+        $('#textarea'+ i).addClass('future')
     } else {
-        $('#'+ i).addClass('past')
+        $('#textarea'+ i).addClass('past')
     }
 }
 
-timeBlocks.addEventListener("click", function (event) {
+// timeBlocks.addEventListener("click", function (event) {
+//     event.preventDefault();
+//     response = timeBlocks.children[event.target.parentElement.id].children[1].value;
+//     console.log(arrayDayPlanner);
+//     arrayDayPlanner[event.target.parentElement.id] = response;
+//     var JSONReadyUsers = JSON.stringify(arrayDayPlanner);
+//     localStorage.setItem("arrayDayPlanner", JSONReadyUsers);
+// });
+
+$('#time-block').on("click", function (event) {
     event.preventDefault();
-    response = timeBlocks.children[event.target.parentElement.id].children[1].value;
-    console.log(arrayDayPlanner);
-    arrayDayPlanner[event.target.parentElement.id] = response;
+    console.log(event.target.id)
+    response = $('#textarea'+ event.target.id).val();
+    console.log(response);
+    arrayDayPlanner[event.target.id] = response;
     var JSONReadyUsers = JSON.stringify(arrayDayPlanner);
     localStorage.setItem("arrayDayPlanner", JSONReadyUsers);
 });
-
