0c4ac2ffcc66c4466eb934595fedff3e90d23b99
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-12 22:12:06 -0400

    Add the hours, rows, and buttons

--- javascript.js
+++ javascript.js
@@ -1,10 +1,18 @@
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 document.getElementById('currentDay').textContent = days[new Date().getDay()] + ", " + new Date().getDate() + " " + months[new Date().getMonth()] + ", " + new Date().getFullYear();
-var timeBlocks = document.getElementById('time-block');
-timeBlocks.appendChild(document.createElement('p'));
-timeBlocks.appendChild(document.createElement('textarea'));
-timeBlocks.appendChild(document.createElement("button"));
-timeBlocks.children[0].classList.add('hour', 'col-1');
-timeBlocks.children[1].classList.add('row', 'col-10');
-timeBlocks.children[2].classList.add('saveBtn', 'col-1');
+
+for (var i = 0; i < 9; i++) {
+    timeBlocks = document.getElementById('time-block').appendChild(document.createElement('div'));
+    timeBlocks.classList.add('container','d-flex', 'justify-content-center')
+    timeBlocks.appendChild(document.createElement('p'));
+    timeBlocks.appendChild(document.createElement('textarea'));
+    timeBlocks.appendChild(document.createElement("button"));
+    timeBlocks.children[0].classList.add('hour', 'col-1');
+    timeBlocks.children[0].textContent = hours[i];
+    timeBlocks.children[1].classList.add('row', 'col-10');
+    timeBlocks.children[2].classList.add('saveBtn', 'col-1');
+}
+
+
