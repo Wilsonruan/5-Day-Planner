cce62385583b7681237cc302e85cfa71876c3ac1
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-12 23:06:46 -0400

    Add color coded to indicate whether it is in a past, present, or future hour.

--- javascript.js
+++ javascript.js
@@ -2,17 +2,26 @@ var months = ["January", "February", "March", "April", "May", "June", "July", "A
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 document.getElementById('currentDay').textContent = days[new Date().getDay()] + ", " + new Date().getDate() + " " + months[new Date().getMonth()] + ", " + new Date().getFullYear();
+// var currentTime = new Date().getHours();
+var currentTime = 12
 
-for (var i = 0; i < 9; i++) {
-    timeBlocks = document.getElementById('time-block').appendChild(document.createElement('div'));
-    timeBlocks.classList.add('container','d-flex', 'justify-content-center')
+for (var i = 9; i < 18; i++) {
+    var timeBlocks = document.getElementById('time-block').appendChild(document.createElement('div'));
+    timeBlocks.classList.add('container','d-flex', 'justify-content-center');
     timeBlocks.appendChild(document.createElement('p'));
     timeBlocks.appendChild(document.createElement('textarea'));
     timeBlocks.appendChild(document.createElement("button"));
     timeBlocks.children[0].classList.add('hour', 'col-1');
-    timeBlocks.children[0].textContent = hours[i];
+    timeBlocks.children[0].textContent = hours[i-9];
     timeBlocks.children[1].classList.add('row', 'col-10');
     timeBlocks.children[2].classList.add('saveBtn', 'col-1');
-}
 
+    if (i == currentTime) {
+        timeBlocks.children[1].classList.add('present')
+    } else if (currentTime < i) {
+        timeBlocks.children[1].classList.add('future')
+    } else {
+        timeBlocks.children[1].classList.add('past')
+    }
+}
 
