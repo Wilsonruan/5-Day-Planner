6fb80d032d57ed57abd235059986a62d2f288634
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-12 23:26:38 -0400

    Adjust the sizing to look good when it is small.

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 document.getElementById('currentDay').textContent = days[new Date().getDay()] + ", " + new Date().getDate() + " " + months[new Date().getMonth()] + ", " + new Date().getFullYear();
 // var currentTime = new Date().getHours();
 var currentTime = 12
+var arrayDayPlanner = JSON.parse(localStorage.getItem("arrayDayPlanner"));
 
 for (var i = 9; i < 18; i++) {
     var timeBlocks = document.getElementById('time-block').appendChild(document.createElement('div'));
@@ -11,10 +12,11 @@ for (var i = 9; i < 18; i++) {
     timeBlocks.appendChild(document.createElement('p'));
     timeBlocks.appendChild(document.createElement('textarea'));
     timeBlocks.appendChild(document.createElement("button"));
-    timeBlocks.children[0].classList.add('hour', 'col-1');
+    timeBlocks.children[0].classList.add('hour', 'col-2', 'col-sm-1');
     timeBlocks.children[0].textContent = hours[i-9];
-    timeBlocks.children[1].classList.add('row', 'col-10');
-    timeBlocks.children[2].classList.add('saveBtn', 'col-1');
+    timeBlocks.children[1].classList.add('row', 'col-8', 'col-sm-10');
+    timeBlocks.children[2].classList.add('saveBtn', 'col-2', 'col-sm-1');
+    timeBlocks.children[2].textContent = "Save";
 
     if (i == currentTime) {
         timeBlocks.children[1].classList.add('present')
