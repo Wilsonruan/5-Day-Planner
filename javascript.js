2dff22b1cf6b979168d7861bf4e2dae68ee5e007
Wilson Ruan <wilsonruan23@gmail.com>
2020-06-16 21:27:27 -0400

    Remove unnecessary lines

--- javascript.js
+++ javascript.js
@@ -1,5 +1,3 @@
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var hours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
 var currentTime = new Date().getHours();
@@ -9,7 +7,7 @@ if (arrayDayPlanner === null) {
     arrayDayPlanner = ["", "", "", "", "", "", "", "",""];
 }
 
-for (var i = 0; i < 9; i++) {
+for (var i = 0; i < arrayDayPlanner.length; i++) {
     $('div').append('<form id="form' + i + '" method="POST" class="container d-flex justify-content-center"> </form>');
     $('#form' + i).append('<label class="hour col-3 col-lg-1 text-left"> <br> <p>' + hours[i] + '</p> </label>');
     $('#form' + i).append('<textarea id="textarea' + i + '" class="row col-7 col-lg-10">' + arrayDayPlanner[i] + '</textarea>');
@@ -25,7 +23,6 @@ for (var i = 0; i < 9; i++) {
 }
 
 $('#time-block').on("click", function (event) {
-    event.preventDefault();
     response = $('#textarea'+ event.target.id).val();
     arrayDayPlanner[event.target.id] = response;
     var JSONReadyUsers = JSON.stringify(arrayDayPlanner);
